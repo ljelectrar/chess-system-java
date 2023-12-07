cbb5cc20317085d1bce986437272d0be1f113594
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-07 18:38:35 -0300

    Board and Piece.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,10 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        Position pos = new Position(1,2);
-
-        System.out.println(pos);
+        Board board = new Board(8,8);
     }
 }
