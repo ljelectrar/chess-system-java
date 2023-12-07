feb65a7d6de68ab20fd7421511d378d9fa7ac106
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-07 18:30:08 -0300

    Class Position.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Position pos = new Position(1,2);
+
+        System.out.println(pos);
     }
 }
